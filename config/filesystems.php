74ee009d64d721e4318f3c6740947d1cd9c39bb2
Jannat Shathi <18103082@iubat.edu>
2021-11-29 00:22:40 +0600

    img

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,7 @@ return [
 
         'public' => [
             'driver' => 'public',
-            'root' => public_path('/'),
+            'root' => public_path('/uploads'),
             'url' => env('APP_URL').'/uploads',
             'visibility' => 'public',
         ],
